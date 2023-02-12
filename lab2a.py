1d146804faec0f2afbebc4353b904c031e783716
dpVaghela <125076310+dpVaghela@users.noreply.github.com>
2023-02-11 19:53:54 -0500

    Create lab2a.py

---
+++ lab2a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+def show_var():
+	s_name = sys.argv[0]
+	arg1 = sys.argv[1]
+	arg2 = sys.argv[2]
+
+	print("Script name:", s_name)
+	print("Arguments:", arg1, arg2)
+	print("Script name and arguments:", s_name, arg1, arg2)
+
+if __name__ == "__main__":
+	show_var()
